06c12b583b787cc2499451890bc7395fa421106b
Eric Krebs <ekrebs@outlook.com>
2022-08-22 01:20:46 -0500

    reworked frontend to mak it easier to support diffrent diffussion models

--- backend/app.py
+++ backend/app.py
@@ -5,26 +5,72 @@ import genImages
 app=Flask(__name__)
 
 
-@app.route("/genimage",methods=["OPTIONS"])
-def genImagePreflight():
-	print('preflight')
-	return '',{
+# TODO Figure out where to set these globally
+optionsPrefilghtResponseHeaders={
+	'Access-Control-Allow-Origin':'http://localhost:3000',
+	'Access-Control-Allow-Methods':'POST,GET,OPTIONS',
+	'Access-Control-Max-Age':2,
+	'Access-Control-Allow-Headers':'Content-Type'
+}
+
+defaultHeaders={
 		'Access-Control-Allow-Origin':'http://localhost:3000',
-		'Access-Control-Allow-Methods':'POST,GET,OPTIONS',
-		'Access-Control-Max-Age':2,
-		'Access-Control-Allow-Headers':'Content-Type'
+		'Access-Control-Allow-Methods':'POST,GET,OPTIONS'
 	}
 
-@app.route("/genimage",methods=["POST"])
+@app.route("/latentDiffusion/genimage",methods=["OPTIONS"])
+def genLatentDiffusionImagesPreflight():
+	print('preflight')
+	return '',optionsPrefilghtResponseHeaders
+
+@app.route("/latentDiffusion/genimage",methods=["POST"])
 @use_args({"prompt":fields.Str(required=True),"scale":fields.Float(),"width":fields.Integer(),"height":fields.Integer(),"numSamples":fields.Integer()})
-def genImage(args):
+def genLatentDiffusionImages(args):
 
 	images=genImages.saveImages(genImages.genLatentDiffusion(prompt=args["prompt"], width=args["width"], height=args["height"], scale=args["scale"],numSamples=args["numSamples"]),"latent-diffusion",genImages.imageDirPath())
 	
 	return {
 		"prompt":args["prompt"],
 		"files":images
-	},{
-		'Access-Control-Allow-Origin':'http://localhost:3000',
-		'Access-Control-Allow-Methods':'POST,GET,OPTIONS'
-	}
+	},defaultHeaders
+
+@app.route("/latentDiffusion/getFormFields",methods=["OPTIONS"])
+def getLatentDiffusionFormDataPreflight():
+	print('preflight')
+	return '',optionsPrefilghtResponseHeaders
+
+
+@app.route("/latentDiffusion/getFormFields",methods=["POST"])
+def getLatentDiffusionFormData():
+	
+	return {
+		"formFieldsData":[
+			{
+				"name":'prompt',
+				"displayName":"Prompt",
+				"defaultValue":"a painting of an ai",
+				"type":"textbox"
+
+			},
+			{
+				"name":'width',
+				"displayName":"Width",
+				"defaultValue":"256"
+			},
+			{
+				"name":'height',
+				"displayName":"height",
+				"defaultValue":"256"
+			},
+			{
+				"name":'scale',
+				"displayName":"scale",
+				"defaultValue":"8"
+			},
+			{
+				"name":'numSamples',
+				"displayName":"Number of pictures",
+				"defaultValue":"6"
+			}
+		]
+	},defaultHeaders
--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,58 +1,243 @@
-import { useState } from "react"
+import { useContext, useEffect, useReducer, createContext } from "react"
 
 function MainPage() {
 
-	const [images,setImages]=useState([]);
 
 	return <>
 
-		
-		{images.length>0&&<DisplayImages images={images}/>}
-		<GenImageForm setImages={setImages}/>
+		<ImageGenForm endpointForFetchingFormData="http://localhost:5000/latentDiffusion/getFormFields" endpointForSubmittingForm="http://localhost:5000/latentDiffusion/genimage"/>
+
 	</>;
 }
 
 
-function GenImageForm({setImages}){
-	const [prompt,setPrompt]=useState('somePrompt');
-	const [height,setHeight]=useState(256);
-	const [width,setWidth]=useState(256);
-	const [scale,setScale]=useState(5.0);
-	const [numSamples,setNumSamples]=useState(4);
-	let fetchImages=async ()=>{
-		let resp=await fetch('http://localhost:5000/genimage',{
-			body:JSON.stringify({
-				prompt:prompt,
-				height:height,
-				width:width,
-				scale:scale,
-				numSamples:numSamples
-			}),
-			method:'POST',
-			headers:{
-				'Content-Type':'application/json'
+const ImageGenFormDispatch =createContext(null);
+
+
+//TODO figure out Names
+function ImageGenFormWithTextFields({formFieldsData,formFieldValues,submitAction}){
+	return (<form onSubmit={async (ev)=>{
+		ev.preventDefault();
+		submitAction();
+	}}>
+			{formFieldsData.map((fieldData)=>{
+				return <ImageGenField key={fieldData.name} name={fieldData.name} displayName={fieldData.displayName} value={formFieldValues[fieldData.name]} type={fieldData.type}/>
+			})}
+
+			<input type="submit" value="Generate Image" />
+	</form>)
+}
+
+function ImageGenField({name,value,displayName,type}){
+
+	const dispatch=useContext(ImageGenFormDispatch);
+	const changeFunc=(ev)=>{
+		dispatch({type:'formField',action:'set',name:name,value:ev.target.value});
+	};
+
+
+	let FieldToUse=TextFieldInput
+	if(type==='textbox'){
+		FieldToUse=TextBoxFieldInput;
+	}
+	
+
+
+	return (
+		<div style={{padding:'0.5rem'}}>
+			<label>{displayName}<br/>
+
+				<FieldToUse value={value} onChange={changeFunc}/>
+
+
+			</label>
+			<br/>
+			<button type="button" onClick={(ev)=>{ 
+
+				if(confirm('Reset '+displayName)){
+					dispatch({type:'formField',action:'reset',name:name});
+				}
+				
+				
+				}} >reset</button>
+		</div>
+		);
+}
+
+
+function TextFieldInput({value,onChange}){
+	return (
+		<input value={value} onChange={onChange} type="text"/>
+	)
+}
+
+
+function TextBoxFieldInput({value,onChange}){
+	return (
+		<textarea value={value} onChange={onChange} type="text"/>
+	)
+}
+
+function formImageReducer(state,data){
+
+	switch(data.type){
+		case 'formField':
+			return formImageFormFieldReducer(state,data);
+		break;
+		case 'loadFormData':
+			return formFieldsPopulateReducer(state,data);
+		break;
+		case 'images':
+			return formImageFormImageReducer(state,data)
+		break;
+		default:
+			return data;
+		break;
+	}
+	return state;
+}
+
+function formImageFormImageReducer(state,data){
+
+	switch(data.action){
+		case 'set':
+			return {...state,
+				images:[...data.value.files]
 			}
+		break;
+		default:
+			return state;
+		break;
+
+	}
+}
+
+function formFieldsPopulateReducer(state,data){
+
+
+	let valuesOfFormFields={};
+	for(let field of data.value.formFieldsData){
+		valuesOfFormFields[field.name]=field.defaultValue;
+	}
+
+	return {
+		...state,
+		images:[],
+		formFieldsData:[...data.value.formFieldsData],
+		formFieldValues:valuesOfFormFields
+	}
+}
+
+function formImageFormFieldReducer(state,data){
+	switch(data.action){
+		case 'set':
+			return {
+				...state,
+				formFieldValues:{
+					...state.formFieldValues
+					,[data.name]:data.value
+				}
+			};
+		break;
+		case 'reset':
+		let matchingFields=state.formFieldsData.filter((field)=>{
+			return field.name===data.name;
+
 		});
-		let dataobj=await resp.json();
-		setImages(dataobj.files)
+		if(matchingFields.length>0){
+			return {
+			...state,
+			formFieldValues:{
+				...state.formFieldValues
+				,
+				[data.name]:matchingFields[0].defaultValue
+			}
+			};
+		}
+		
+		default:
+			return state;
+		break;
+	}
+}
+
+
+
+
+async function getFormDataFromEndpoint({dispatch,endpointForFetchingFormData}){
+
+	let resp=await fetch(endpointForFetchingFormData,{
+		method:'POST',
+		headers:{
+			'Content-Type':'application/json'
+		}
+	});
+	let formData=await resp.json();
+
+	dispatch({type:'loadFormData',value:formData})
+};
+
+async function submitPromptAndParameters({formFieldsData,formFieldValues,endpointForSubmittingForm,dispatch}){
+
+	let jsonForSubmiting={};
+	for (let field of formFieldsData){
+		jsonForSubmiting[field.name]=formFieldValues[field.name];
 	}
 
 
+
+	let resp=await fetch(endpointForSubmittingForm,{
+		method:'POST',
+		body:JSON.stringify(jsonForSubmiting,null,'\t'),
+		headers:{
+			'Content-Type':'application/json'
+		}
+	});
+	let imgData=await resp.json();
+
+
+
+	dispatch({type:'images',action:'set',value:imgData});
+}
+
+
+function ImageGenForm({ endpointForFetchingFormData,endpointForSubmittingForm}){
+	const [state,dispatch]=useReducer(formImageReducer,{images:[],formFieldValues:{},formFieldsData:[]})
+
+	useEffect(()=>{
+		getFormDataFromEndpoint({dispatch:dispatch,endpointForFetchingFormData:endpointForFetchingFormData})
+		
+		
+	},[endpointForFetchingFormData]);
+
 	return <>
-		<textarea onChange={(ev)=>{ setPrompt(ev.target.value) }} value={prompt}/>
-		Width:<input value={width} onChange={(ev)=>{ setWidth(ev.target.value)}}/>
-		Height:<input value={height} onChange={(ev)=>{ setHeight(ev.target.value)}}/>
-		scale:<input value={scale} onChange={(ev)=>{ setScale(ev.target.value)}}/>
-		numSamples:<input value={numSamples} onChange={(ev)=>{ setNumSamples(ev.target.value)}}/>
-		<button onClick={fetchImages}>Click to make image</button>
-	</>;
+	
+		<ImageGenFormDispatch.Provider value={dispatch}>
+
+		{state.images.length>0&&<DisplayImages images={state.images}/>}
+			<ImageGenFormWithTextFields 
+				formFieldValues={state.formFieldValues} 
+				formFieldsData={state.formFieldsData} 
+				submitAction={
+					()=>{ 
+						submitPromptAndParameters({
+							dispatch:dispatch,
+							formFieldsData:state.formFieldsData,
+							formFieldValues:state.formFieldValues,
+							endpointForSubmittingForm
+						});
+
+					}
+				}/>
+		</ImageGenFormDispatch.Provider>
+	</>
 
 }
+
 
 
 
 function DisplayImages({images}){
-	console.log(images);
 	
 	let genImages=images.map((img)=>{
 		let imgUrlBase=new URL('http://localhost:5000/');
